ada3a47cb63992362de36f4235ebecd1d7e57011
Stefano Ruth <stefano6262@gmail.com>
2019-05-18 17:33:19 +0200

    wip

--- tests/Option.test.ts
+++ tests/Option.test.ts
@@ -14,4 +14,20 @@ describe('Option', () => {
 	test('ArrayModeWithoutValue', () => {
 		expect(() => new Option('foo', 'f', undefined, OptionMode.isArray)).toThrow()
 	})
+
+	test('Shortcut', () => {
+		option = new Option('foo', 'f')
+		expect(option.getShortcut()).toBe('f')
+
+		option = new Option('foo', '-f|-ff|fff')
+		expect(option.getShortcut()).toBe('f|ff|fff')
+
+		option = new Option('foo', ['f', 'ff', '-fff'])
+		expect(option.getShortcut()).toBe('f|ff|fff')
+
+		option = new Option('foo')
+		expect(option.getShortcut()).toBe(null)
+	})
+
+	//
 })
