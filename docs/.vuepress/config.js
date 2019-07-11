19de53118b8043cdfa13337ef44ba0451b43ed2e
Stefano Ruth <stefano6262@gmail.com>
2019-07-11 22:15:11 +0200

    wip

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'Console',
+	description: 'Just playing around',
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Guide', link: '/setup/' },
+			{ text: 'External', link: 'https://google.com' },
+		],
+		sidebar: 'auto',
+	},
+}
